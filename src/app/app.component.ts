3fa9a68b2dd1204c5ac5f6644b3ed849135569f2
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-10 10:58:11 -0400

    using keyframes for animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -60,7 +60,35 @@ import { Component } from '@angular/core';
         }))
       ])
     ]
-    )
+    ),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', animate(1000, keyframes([
+        style({
+          transform: 'translateX(-100px)',
+          opacity: 0,
+          offset: 0
+        }),
+        style({
+          transform: 'translateX(-50px)',
+          opacity: 0.5,
+          offset: 0.3
+        }),
+        style({
+          transform: 'translateX(-20px)',
+          opacity: 0.2,
+          offset: 0.8
+        }),
+        style({
+          transform: 'translateX(0px)',
+          opacity: 1,
+          offset: 1
+        })
+      ])))
+    ])
   ]
 })
 export class AppComponent {
